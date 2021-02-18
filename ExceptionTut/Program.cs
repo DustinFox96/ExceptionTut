c1507fc397ae3747fc26e71f1bf27a086a53eb60
Dustin Fox <dustinfox1996@gmail.com>
2021-02-18 14:29:12 -0500

    Add project files.

---
+++ ExceptionTut/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExceptionTut {
+    class Program {
+        static void Main(string[] args) {
+            try {
+
+                Level1(3);
+            } catch (Exception ex) {
+                Console.WriteLine($"The program encountered an exception");
+                Console.WriteLine($"Exception: {ex.Message}");
+
+
+            }
+            Console.WriteLine("done . . .");
+        }
+        static void Level1(int i) {
+            Level2(i - 1);
+
+        }
+        static void Level2(int i) {
+            Level3(i - 2);
+        }
+        static void Level3(int i) {
+            var result = 100 / i;
+            Console.WriteLine($"result is {result}");
+            
+
+             
+
+            
+        }
+    }
+}
+
